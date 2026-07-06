99e8789264a4beb366e523b8a442b27fc2419c7f
david <d8087869@gmail.com>
2026-07-06 16:55:01 +0300

    4-5

--- recursion_exercises.py
+++ recursion_exercises.py
@@ -22,3 +22,26 @@ def numbers_to_n(n):
     else:
         return numbers_to_n(n -1)+[n]
 print(numbers_to_n(5))    
+
+#4
+def count_items(lst):
+    if lst == []:
+        return 0
+    else : 
+        return 1+ count_items(lst[1:])
+print(count_items(['a','b','c']))
+
+#5
+def count_evens(numbers):
+    if len (numbers) ==0:
+        return 0 
+    else:
+        first = numbers[0]
+        rest = numbers[1:]
+        if first % 2 == 0:
+            return 1 + count_evens(rest)
+        else:
+            return count_evens(rest)
+print(count_evens([4, 7, 10, 3, 8]))        
+
+#6
